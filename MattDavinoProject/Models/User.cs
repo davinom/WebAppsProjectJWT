eb82fd2b9703234ccdcb44bac967762313edb4aa
Matt Davino <davinom@go.stockton.edu>
2018-05-01 17:10:24 -0400

    ProjectJWT

---
+++ MattDavinoProject/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MattDavinoProject.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
